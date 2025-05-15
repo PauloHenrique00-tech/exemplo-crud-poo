b4c2466962a9be3d146607b27af9d5c45b041685
PauloHenrique00-tech <saraiva_ph@hotmail.com>
2025-05-15 09:56:44 -0300

    Update visualizar.php

--- produtos/visualizar.php
+++ produtos/visualizar.php
@@ -34,8 +34,8 @@ $quantidade = count($listaDeProdutos)
           <?php foreach ($listaDeProdutos as $produto): ?>
             <div class="col-sm-6">
                 <article class="bg-body-secondary p-2">
-                    <h3><?=$listaDeProdutos["produto"]?></h3>
-                    <h4><?=$listaDeProdutos["fabricante"]?></h4>
+                    <h3><?=$produto["produto"]?></h3>
+                    <h4><?=$produto["fabricante"]?></h4>
                     <p><b>Preço: </b> <?=Utils::formatarPreco($produto["preco"])?></p>
                     <p><b>Quantidade: </b> <?=$produto["quantidade"]?></p>
                     <p><b>Total: </b><?=Utils::formatarPreco($produto["preco"] * $produto["quantidade"])?></p>
